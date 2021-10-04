faef19a9a7a8c81234c79486cb699514fd79013f
FilimonenkovS <filimonenkov2013@mail.ru>
2021-10-04 16:15:33 +0300

    first commit

---
+++ SR5Filimonenkov.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a -"))
+b = int(input("Enter b -"))
+c = int(input("Enter c -"))
+d = int(input("Enter d -"))
+
+if (a<c) and (b<d) or (b<c) and (a<d):
+    print("Da, mozhno")
+else:
+    print("Net")
+    
+                       
